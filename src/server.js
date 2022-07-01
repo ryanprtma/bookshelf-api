91be0165c2c946b5a7b112ef3313d903a3b022d2
Ryan Apratama <ryanapratama16@gmail.com>
2022-07-02 00:47:09 +0700

    HTTP server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('server test');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server run at ${server.info.uri}`);
+};
+
+init();

59f2a7947c4f7dd6544b75afd3f01d06192127ad
Ryan Apratama <ryanapratama16@gmail.com>
2022-07-02 23:00:49 +0700

    Mengubah data buku

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,3 @@
-/* eslint-disable array-callback-return */
 const { nanoid } = require('nanoid');
 const books = require('./books');
 
@@ -172,4 +171,66 @@ const getBookByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
+const editBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+
+  const updatedAt = new Date().toISOString();
+  const index = books.findIndex((book) => book.id === id);
+  const isValidPage = readPage <= pageCount;
+  const finished = pageCount === readPage;
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (!isValidPage) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (index !== -1 && name !== undefined && isValidPage) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      finished,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler,
+};
